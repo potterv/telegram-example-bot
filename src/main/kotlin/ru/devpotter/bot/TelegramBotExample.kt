9138ad1078c1a59fe4be8ce87d4f819f7cf907f3
Владимир Гончар <gonchar@092.pfr.ru>
2022-07-18 17:04:55 +0300

    added inlinebutton

--- src/main/kotlin/ru/devpotter/bot/TelegramBotExample.kt
+++ src/main/kotlin/ru/devpotter/bot/TelegramBotExample.kt
@@ -1,21 +1,27 @@
 package ru.devpotter.bot
 
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+//import org.telegram.telegrambots.meta.api.objects
 
 @Service
 class TelegramBotExample : TelegramLongPollingBot() {
 
-    //init {
+//    init {
 //    options.proxyType= DefaultBotOptions.ProxyType.HTTP
 //    options.proxyHost="10.92.192.50"
 //    options.proxyPort = 3128
 //
 //}
+
     //    @Value("\${telegram.botName}")
     private val botName: String = "PotterVV_bot"
 
@@ -38,6 +44,8 @@ class TelegramBotExample : TelegramLongPollingBot() {
                 val messageTextbot = message.text
                 when {
                     messageTextbot == "/start" -> "Добро пожаловать!"
+                    messageTextbot.startsWith("кнопка")-> "Вы нажали кнопку"
+                    messageTextbot.startsWith("Hello")-> "Вы нажали кнопку inlineButton"
                     else -> "Вы написали: *$messageTextbot*"
                 }
             } else {
@@ -52,21 +60,39 @@ class TelegramBotExample : TelegramLongPollingBot() {
         responseMessage.enableMarkdownV2(true)
         responseMessage.replyMarkup = getReplayMurkup(
             listOf(
-                listOf("кнопка 1", "кнопка 2"),
-                listOf("кнопка 1", "кнопка 2")
+                listOf("Обратнаясвязь", "О боте"),
+                listOf("кнопка 3", "кнопка 4")
             )
         )
+//        responseMessage.replyMarkup = getInlineKeyboardMurkup(
+//            listOf(
+//                listOf(InlineKeyboardButton("1"), InlineKeyboardButton("2")),
+//                listOf(InlineKeyboardButton("кнопка 5"), InlineKeyboardButton("кнопка 6"))
+//            )
+//        )
+
+        responseMessage.replyMarkup =InlineKeyboardMarkup(listOf(listOf(InlineKeyboardButton("General Kenobi"))))
         execute(responseMessage)
     }
 
     private fun getReplayMurkup(allButtons: List<List<String>>): ReplyKeyboardMarkup {
         val murkup = ReplyKeyboardMarkup()
         murkup.keyboard = allButtons.map{rowButtons ->
-            val row =KeyboardRow()
+            val row = KeyboardRow()
         rowButtons.forEach{rowButton -> row.add(rowButton.toString())}
         row
         }
         return murkup
     }
+
+//    private fun getInlineKeyboardMurkup(allButtons: List<List<InlineKeyboardButton>>):InlineKeyboardMarkup{
+//        val inlineMurkup = InlineKeyboardMarkup()
+//        inlineMurkup.keyboard = allButtons.map{ rowButtons ->
+//           val row = KeyboardRow()
+//           rowButtons.forEach{rowButton -> row.add(rowButton.toString())}
+//           row
+//        }
+//        return inlineMurkup
+//    }
 
 }
